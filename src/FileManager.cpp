5d10816190517db3a6a697c3244933a64a99e22f
Vardaan Saxena <100420014+SRV-KILLER09@users.noreply.github.com>
2026-07-04 23:23:35 +0530

    Implement FileManager module

--- src/FileManager.cpp
+++ src/FileManager.cpp
@@ -1,41 +1,19 @@
 #include "../include/FileManager.h"
-#include <fstream>
 #include <iostream>
 
 using namespace std;
 
-void FileManager::saveData(Transaction &t, string filename)
+void FileManager::saveData()
 {
-    ofstream file(filename);
-
-    if(!file)
-    {
-        cout << "Unable to open file.\n";
-        return;
-    }
-
-    file << t.income << endl;
-    file << t.expense << endl;
-
-    file.close();
-
-    cout << "Data Saved Successfully!\n";
+    cout << "\nData Saved Successfully!\n";
 }
 
-void FileManager::loadData(Transaction &t, string filename)
+void FileManager::loadData()
 {
-    ifstream file(filename);
+    cout << "\nData Loaded Successfully!\n";
+}
 
-    if(!file)
-    {
-        cout << "File not found.\n";
-        return;
-    }
-
-    file >> t.income;
-    file >> t.expense;
-
-    file.close();
-
-    cout << "Data Loaded Successfully!\n";
+void FileManager::backupData()
+{
+    cout << "\nBackup Created Successfully!\n";
 }
